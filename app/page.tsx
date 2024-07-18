7ced65a23f2d5d27b39247f7dd3708ea8030ccb6
alirioceraficajr <127605167+aliriocerafica@users.noreply.github.com>
2024-07-18 21:57:03 +0800

    animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,46 @@
-"use client";
+'use client'
 import React, { useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 import { InView } from "react-intersection-observer";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import Education from "./components/education";
 import Contact from "./components/contact";
-import Image from "next/image";
 import Certificate from "./components/certificate";
 import Works from "./components/works";
-import "./css/home.css";
 import Tech from "./components/tech";
+import "./css/home.css";
+import Image from "next/image";
 import { ReactTyped } from "react-typed";
-
-const animationVariants = {
-  hidden: { opacity: 0, x: -100 },
-  visible: { opacity: 1, x: 0 },
-};
 
 const Page = () => {
   useEffect(() => {
-    // Add a class to trigger the animation on refresh
+ 
     window.scrollTo(0, 0);
     document.body.classList.add("fade-in");
+
+    const handleScroll = () => {
+      const elements = document.querySelectorAll(".animated");
+      elements.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        const isInView = rect.top <= window.innerHeight && rect.bottom >= 0;
+        if (isInView) {
+          element.classList.add("visible");
+        } else {
+          element.classList.remove("visible");
+        }
+      });
+    };
+
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
-  const handleContextMenu = (e: { preventDefault: () => void }) => {
+  const handleContextMenu = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-    // Optionally, you can alert or inform the user that inspecting is disabled
   };
 
   useEffect(() => {
@@ -54,18 +67,18 @@ const Page = () => {
             </div>
             <div className="md:order-1 text-white mt-20 gradient-text">
               <motion.h2
-                className="text-5xl font-bold mb-2 "
+                className="text-5xl font-bold mb-2 animated"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
+                transition={{ duration: 1 }}
               >
                 Hi, I'm Ali Cerafica
               </motion.h2>
               <motion.h2
-                className="text-2xl font-bold mb-2"
+                className="text-2xl font-bold mb-2 animated"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.2 }}
+                transition={{ duration: 1, delay: 1 }}
               >
                 <span style={{ display: "inline-block" }}>
                   <h1
@@ -96,10 +109,10 @@ const Page = () => {
                 />
               </motion.h2>
               <motion.p
-                className="text-gray-400 mt-4 text-base font-normal"
+                className="text-gray-400 mt-4 text-base font-normal animated"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.4 }}
+                transition={{ duration: 0.5, delay: 1 }}
               >
                 I am a recent graduate of Bachelor of Science in Computer
                 Science, specializing in Application Development, from The
@@ -114,7 +127,7 @@ const Page = () => {
                 transition={{ duration: 0.5, delay: 0.6 }}
               >
                 <motion.p
-                  className="font-normal border-2 mt-10 text-white w-32 text-center border-[#7484dd] hover:bg-[#4855a3] hover:text-white hover:border-white p-2 text-sm rounded-lg"
+                  className="font-normal border-2 mt-10 text-white w-32 text-center border-[#7484dd] hover:bg-[#4855a3] hover:text-white hover:border-white p-2 text-sm rounded-lg animated"
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                 >
@@ -122,7 +135,7 @@ const Page = () => {
                 </motion.p>
               </motion.a>
               <motion.div
-                className="container-about flex items-start mt-48"
+                className="container-about flex items-start mt-48 animated"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.8 }}
@@ -181,10 +194,14 @@ const Page = () => {
             <motion.div
               ref={ref}
               id="about"
-              className="about mx-auto w-3/5 p-2 text-2xl gradient-text text-center font-bold mt-20"
+              className="about mx-auto w-3/5 p-2 text-2xl gradient-text text-center font-bold mt-20 animated"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              variants={{
+                visible: { opacity: 1, y: 0 },
+                hidden: { opacity: 0, y: 2 },
+              }}
+              transition={{ duration: 2 }}
             >
               <h1>About Me</h1>
               <div className="w-16 mx-auto border-b-4 border-white-400 mt-2 rounded-full"></div>
@@ -195,15 +212,25 @@ const Page = () => {
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
-              className="contentAbout w-auto bg-white mt-12"
+              className="contentAbout w-auto bg-white mt-12 animated"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              variants={{
+                visible: { opacity: 1, y: 0 },
+                hidden: { opacity: 0, y: 50 },
+              }}
+              transition={{ duration: 0.5 }}
             >
               <div className="content w-full lg:w-3/5 h-auto mx-auto flex flex-col lg:flex-row-reverse p-8">
                 <motion.div
                   className="w-full lg:w-1/2 flex justify-center lg:justify-start items-center mt-10"
-                  whileHover={{ scale: 1.1 }} // Zoom effect on hover
+                  initial="hidden"
+                  animate={inView ? "visible" : "hidden"}
+                  variants={{
+                    visible: { opacity: 1 },
+                    hidden: { opacity: 0 },
+                  }}
+                  transition={{ duration: 2 }}
                 >
                   <Image
                     src="/mySet.png"
@@ -214,9 +241,18 @@ const Page = () => {
                   />
                 </motion.div>
                 <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-                  <div className="text-content px-4 lg:px-0 text-center lg:text-left">
+                  <motion.div
+                    className="text-content px-4 lg:px-0 text-center lg:text-left"
+                    initial="hidden"
+                    animate={inView ? "visible" : "hidden"}
+                    variants={{
+                      visible: { opacity: 1, y: 0 },
+                      hidden: { opacity: 0, y: 50 },
+                    }}
+                    transition={{ duration: 0.5 }}
+                  >
                     <h2 className="text-2xl font-bold mt-12 gradient-text mb-6">
-                     Front End Developer
+                      Front End Developer
                     </h2>
                     <p className="mt-2 mb-12 text-gray-600 text-base font-normal">
                       A recent graduate with a Bachelor of Science in Computer
@@ -224,88 +260,126 @@ const Page = () => {
                       web applications. I focus on developing prototypes and
                       mockups using Figma, and I have proficient skills in
                       design. I also develop the front end of mobile and web
-                      applications using various programming languages
+                      applications using various programming languages.
                     </p>
-                  </div>
+                  </motion.div>
                 </div>
               </div>
             </motion.div>
           )}
         </InView>
-
         <InView triggerOnce>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
-              id=""
-              className="works w-full h-auto mx-auto mt-10 mb-28"
+              className="contentAbout w-auto bg-white mt-12 animated"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              exit="exit"
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              {/* Your existing content */}
+            </motion.div>
+          )}
+        </InView>
+        <InView triggerOnce>
+          {({ inView, ref }) => (
+            <motion.div
+              ref={ref}
+              id="education"
+              className="works w-full h-auto mx-auto mt-10 mb-28 animated"
+              initial="hidden"
+              animate={inView ? "visible" : "hidden"}
+              exit="exit"
+              variants={{
+                visible: { opacity: 1, y: 0 },
+                hidden: { opacity: 0, y: 50 },
+              }}
+              transition={{ duration: 0.5 }}
             >
               <Education />
             </motion.div>
           )}
         </InView>
-
+        {/* Repeat InView sections for Works, Tech, Certificate, Contact, Footer */}
+        {/* Example for Works */}
         <InView triggerOnce>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
               id="Works"
-              className="works w-full h-auto mx-auto mt-10 mb-28"
+              className="works w-full h-auto mx-auto mt-10 mb-28 animated"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              exit="exit"
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
             >
               <Works />
             </motion.div>
           )}
         </InView>
-       <div  id="tech1" className="mt-20">
-       <div className="text-center text-2xl gradient-text font-bold animate-pulse">
-              <h1>
-                Tech Skills{' '}
-                <Image
-                  src="/dev/Gear.svg"
-                  alt="Code Icon"
-                  width={32}
-                  height={32}
-                  className="ml-2 inline w-8 h-8 spin text-white "
-                />
-                <div className="w-16 mx-auto border-b-4 border-white-400 mt-2 rounded-full "></div>
-              </h1>
-            </div>
-       <InView triggerOnce>
-          {({ inView, ref }) => (
-            <motion.div
-              ref={ref}
-             
-              className="works w-full h-auto mx-auto  mb-28"
-              initial="hidden"
-              animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
-            >
-              <Tech />
-            </motion.div>
-          )}
-        </InView>
-       </div>
-    
+        {/* Example for Tech */}
+        <div id="tech1" className="mt-20">
+          <div className="text-center text-2xl gradient-text font-bold animate-pulse">
+            <h1>
+              Tech Skills{" "}
+              <Image
+                src="/dev/Gear.svg"
+                alt="Code Icon"
+                width={32}
+                height={32}
+                className="ml-2 inline w-8 h-8 spin text-white "
+              />
+              <div className="w-16 mx-auto border-b-4 border-white-400 mt-2 rounded-full "></div>
+            </h1>
+          </div>
+          <InView triggerOnce>
+            {({ inView, ref }) => (
+              <motion.div
+                ref={ref}
+                className="works w-full h-auto mx-auto  mb-28 animated"
+                initial="hidden"
+                animate={inView ? "visible" : "hidden"}
+                variants={{
+                  visible: { opacity: 1, y: 0 },
+                  hidden: { opacity: 0, y: 50 },
+                }}
+                transition={{ duration: 0.5 }}
+              >
+                <Tech />
+              </motion.div>
+            )}
+          </InView>
+        </div>
 
+        {/* Example for Certificate */}
         <InView triggerOnce>
           {({ inView, ref }) => (
             <motion.div
               ref={ref}
-              id="cert"
+              id="certificate"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              exit="exit"
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
             >
               <Certificate />
             </motion.div>
           )}
         </InView>
+        {/* Example for Contact */}
         <InView triggerOnce>
           {({ inView, ref }) => (
             <motion.div
@@ -313,7 +387,12 @@ const Page = () => {
               id="contact"
               initial="hidden"
               animate={inView ? "visible" : "hidden"}
-              variants={animationVariants}
+              exit="exit"
+              variants={{
+                visible: { opacity: 1, y: 0 },
+                hidden: { opacity: 0, y: 50 },
+              }}
+              transition={{ duration: 0.5 }}
             >
               <Contact />
             </motion.div>
