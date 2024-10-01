5c1c022e2e51cbbc4d8e564a10ce8c5877c3f660
alirioceraficajr <127605167+aliriocerafica@users.noreply.github.com>
2024-10-01 11:52:34 +0800

    v2 locker

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
-import Link from 'next/link';
-import { HomeIcon, LockClosedIcon, UserIcon, CogIcon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
 
-// Example Sidebar Component
 const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  // Handle resize events to auto-close the sidebar
+  // Auto-close sidebar on small screens
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 640) {
@@ -22,102 +20,132 @@ const Sidebar: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex">
-      {/* Sidebar */}
+    <div className="relative flex pl-4">
+      {/* External Toggle Button that moves with the Sidebar */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`fixed top-2 transform transition-transform duration-300 bg-black text-white rounded-full p-2 shadow-lg focus:outline-none z-50
+          ${isOpen ? "translate-x-64" : "translate-x-0"}
+        `}
+      >
+        {/* Toggle Icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fill-rule="evenodd" d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+  <path fill-rule="evenodd" d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+</svg>
+
+      </button>
+
+      {/* Sidenav */}
       <nav
-        className={`fixed top-0 left-0 h-full bg-black w-80 pl-4 text-white shadow-lg transition-transform duration-300 transform ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
+        className={`fixed top-0 left-0 h-full bg-black w-64 pl-4 text-white shadow-lg transition-transform duration-300 transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
-        style={{ zIndex: 50 }}  // Ensure the sidebar is always on top
+        style={{ zIndex: 40 }}
       >
         <div className="flex justify-between items-center p-4">
-          <Link href="/" passHref>
+          {/* <Link href="/" passHref>
             <h1 className="font-semibold cursor-pointer pt-4">
               <span className="text-2xl">ali</span>
               <span className="text-red-white font-medium text-3xl">DEV</span>
             </h1>
-          </Link>
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="px-2 py-2 rounded focus:outline-none pt-2"
-          >
-            {/* Open Button */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 20 20" fill="none" height="32" width="32">
-              <desc>Text Line Streamline Icon</desc>
-              <path
-                stroke="#FFFFFF"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M1.06875 3.825H13.931249999999999M1.06875 7.5H13.931249999999999M1.06875 11.175H6.58125"
-                strokeWidth="1"
-              />
-            </svg>
-          </button>
+          </Link> */}
         </div>
+
         <div className="flex flex-col space-y-8 p-4 mt-4">
-          <Link href="/" passHref>
+        <Link href="/" passHref>
             <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
-              <HomeIcon className="h-6 w-6 mr-2" />
-              <span className="cursor-pointer w-full">Home</span>
+              {/* Settings Icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fill-rule="evenodd" d="M9.53 2.47a.75.75 0 0 1 0 1.06L4.81 8.25H15a6.75 6.75 0 0 1 0 13.5h-3a.75.75 0 0 1 0-1.5h3a5.25 5.25 0 1 0 0-10.5H4.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+</svg>
+
+
+
+              {isOpen && <span className="ml-3">Portfolio</span>}
+            </div>
+          </Link>
+          <Link href="/bitlock" passHref>
+            <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
+              {/* Home Icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 w-6 h-6"
+              >
+                <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+              </svg>
+
+              {isOpen && <span className="ml-3">Home</span>}
+            </div>
+           
+          </Link>
+          <Link href="/form" passHref>
+            <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
+              {/* Bitlocker Icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              {isOpen && <span className="ml-3">Bitlocker</span>}
             </div>
           </Link>
           <Link href="/" passHref>
             <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
-              <LockClosedIcon className="h-6 w-6 mr-2" />
-              <span className="cursor-pointer w-full">Bitlocker</span>
+              {/* User Icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+
+              {isOpen && <span className="ml-3">User</span>}
             </div>
           </Link>
           <Link href="/" passHref>
             <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
-              <UserIcon className="h-6 w-6 mr-2" />
-              <span className="cursor-pointer w-full">User</span>
-            </div>
-          </Link>
-          <Link href="/" passHref>
-            <div className="w-full flex items-center p-2 hover:bg-white hover:bg-opacity-20 rounded transition">
-              <CogIcon className="h-6 w-6 mr-2" />
-              <span className="cursor-pointer w-full">Settings</span>
+              {/* Settings Icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 w-6 h-6"
+              >
+                <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 1 0 4.5 2.25 2.25 0 0 1 0-4.5ZM9 18.75a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5Z" />
+              </svg>
+
+              {isOpen && <span className="ml-3">Settings</span>}
             </div>
           </Link>
         </div>
       </nav>
 
       {/* Main Content */}
-      <div className={`flex-grow ml-0 transition-all duration-300 ${isOpen ? 'ml-64' : 'ml-0'}`} style={{ zIndex: 1 }}>
-        {/* Top Navigation Bar */}
-        <div className="flex items-center justify-between bg-black text-white shadow-md p-4">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="px-2 py-2 rounded focus:outline-none"
-          >
-            {/* Open Button */}
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="-0.5 -0.5 20 20" fill="none" height="32" width="32">
-              <desc>Text Line Streamline Icon</desc>
-              <path
-                stroke="#FFFFFF"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M1.06875 3.825H13.931249999999999M1.06875 7.5H13.931249999999999M1.06875 11.175H6.58125"
-                strokeWidth="1"
-              />
-            </svg>
-          </button>
-
-          {/* Search Bar and Profile Icon */}
-          <div className="flex items-center space-x-4">
-            <div className="flex items-center border border-gray-300 rounded-lg p-2">
-              <input
-                type="text"
-                placeholder="Search..."
-                className="outline-none w-64 bg-transparent text-white placeholder-gray-400"
-              />
-              <MagnifyingGlassIcon className="h-6 w-6 text-white" />
-            </div>
-            <UserCircleIcon className="h-8 w-8 text-gray-300 hover:text-gray-100 cursor-pointer" />
-          </div>
-        </div>
-
-        {/* Main Content Area */}
+      <div
+        className={`flex-grow transition-all duration-300 ${
+          isOpen ? "ml-64" : "ml-0"
+        }`}
+        style={{ zIndex: 1 }}
+      >
+        {/* Your main content will go here */}
       </div>
     </div>
   );
---
+++ app/form/page.tsx
@@ -0,0 +1,238 @@
+"use client";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import Sidebar from "../components/nav";
+import React from "react";
+
+export default function Page() {
+  const [deviceType, setDeviceType] = useState("");
+  const [deviceNumber, setDeviceNumber] = useState("");
+  const [key, setKey] = useState("");
+  const [isKeyGenerated, setIsKeyGenerated] = useState(false);
+
+  const generatedKeys = new Set();
+
+  const generateUniqueKey = () => {
+    let uniqueKey = "";
+    let lastDigit = -1;
+
+    for (let i = 0; i < 10; i++) {
+      let digit;
+      do {
+        digit = Math.floor(Math.random() * 10);
+      } while (digit === lastDigit);
+
+      uniqueKey += digit;
+      lastDigit = digit;
+    }
+
+    return uniqueKey;
+  };
+
+  const generateKey = () => {
+    const deviceID = parseInt(deviceNumber, 10);
+    if (!isNaN(deviceID) && deviceID > 0 && deviceID <= 1000) {
+      let newKey;
+      do {
+        newKey = generateUniqueKey();
+      } while (
+        generatedKeys.has(`${deviceType}-${deviceID} - "${newKey}"`) &&
+        generatedKeys.size < 1000
+      );
+
+      if (generatedKeys.size >= 1000) {
+        alert("Maximum unique keys generated for the device type.");
+        return;
+      }
+
+      generatedKeys.add(`${deviceType}-${deviceID} - "${newKey}"`);
+      setKey(`${deviceType}-${deviceID} - "${newKey}"`);
+      setIsKeyGenerated(true);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Invalid Device ID",
+        text: "Please enter a valid device ID number (1-1000).",
+      });
+    }
+  };
+
+  const resetForm = () => {
+    setDeviceType("");
+    setDeviceNumber("");
+    setKey("");
+    setIsKeyGenerated(false);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(key)
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          title: "Copied!",
+          text: "Key copied to clipboard!",
+        });
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Failed to copy key!",
+        });
+      });
+  };
+
+  const downloadKeyAsText = () => {
+    if (key) {
+      const blob = new Blob([key], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `BitLocker_TPM_Key_${deviceNumber}.txt`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "No Key to Download",
+        text: "Please generate a key before downloading.",
+      });
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (isKeyGenerated) {
+      resetForm();
+    } else {
+      generateKey();
+    }
+  };
+
+  // New onDelete function to reset the key
+  const onDelete = () => {
+    resetForm();
+    Swal.fire({
+      icon: "info",
+      title: "Key Deleted",
+      text: "The generated key has been deleted.",
+    });
+  };
+
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <Sidebar />
+      <div className="flex-1 flex justify-center items-center p-4 lg:p-10">
+        <div className="form bg-white p-4 rounded-2xl bg-opacity-20 shadow-sky-100 shadow-opacity-20 shadow-lg border-1 border-gray-400">
+          <div className="backdrop-blur-md bg-[#1e2330] bg-opacity-80 shadow-lg rounded-lg p-8 max-w-2xl w-full shadow-opacity-60">
+            <h1 className="text-3xl font-normal text-center mb-6 text-white">
+              BITLOCKER KEY GENERATORS APABS
+              <hr className="border-t-1 border-white w-full mt-2" />
+            </h1>
+            <div className="border-t- border-gray-200 rounded-full w-full"></div>
+
+            <div className="mb-6">
+              <label className="block text-sm font-normal mb-1 text-white">
+                Select Device Type:
+              </label>
+              <select
+                className="text-white bg-gray-500 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-sky-500"
+                onChange={(e) => setDeviceType(e.target.value)}
+                value={deviceType}
+                aria-label="Device Type"
+              >
+                <option value="">Select...</option>
+                <option value="APABS-LAPTOP">Laptop</option>
+                <option value="APABS-DESKTOP">Desktop</option>
+              </select>
+            </div>
+            <div className="mb-6">
+              <label className="block text-sm font-normal mb-1 text-white">
+                Device ID Number:
+              </label>
+              <input
+                type="number"
+                placeholder="Enter Device ID Number"
+                className="text-white bg-gray-500 rounded-md p-2 w-full focus:outline-none focus:ring-2 focus:ring-sky-500"
+                onChange={(e) => setDeviceNumber(e.target.value)}
+                value={deviceNumber}
+                aria-label="Device ID Number"
+              />
+            </div>
+            <button
+              onClick={handleButtonClick}
+              className="bg-black text-white p-3 rounded-md w-full hover:bg-gray-800 transition duration-200"
+            >
+              {isKeyGenerated ? "Generate New Key" : "Generate Key"}
+            </button>
+            <div className="mt-6">
+              {key && (
+                <>
+                  <h2 className="text-xl font-medium text-white mb-2">
+                    Generated Key:
+                  </h2>
+                  <div className="border text-blue-400 font-semibold border-white rounded-md p-2 mb-1 flex justify-between items-center">
+                    <div className="flex items-center">
+                      <span>{key}</span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      {" "}
+                      {/* Updated spacing here */}
+                      <button
+                        onClick={onDelete}
+                        className="flex items-center justify-center bg-gray-200 text-black p-1 rounded-md hover:bg-gray-300 transition duration-200"
+                        aria-label="Delete Item"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                      <button
+                        onClick={copyToClipboard}
+                        className="flex items-center justify-center bg-gray-200 text-black p-1 rounded-md hover:bg-gray-300 transition duration-200"
+                        aria-label="Copy Key to Clipboard"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="size-5"
+                        >
+                          <path d="M7 3.5A1.5 1.5 0 0 1 8.5 2h3.879a1.5 1.5 0 0 1 1.06.44l3.122 3.12A1.5 1.5 0 0 1 17 6.622V12.5a1.5 1.5 0 0 1-1.5 1.5h-1v-3.379a3 3 0 0 0-.879-2.121L10.5 5.379A3 3 0 0 0 8.379 4.5H7v-1Z" />
+                          <path d="M4.5 6A1.5 1.5 0 0 0 3 7.5v9A1.5 1.5 0 0 0 4.5 18h7a1.5 1.5 0 0 0 1.5-1.5v-5.879a1.5 1.5 0 0 0-.44-1.06L9.44 6.439A1.5 1.5 0 0 0 8.378 6H4.5Z" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <button
+  onClick={downloadKeyAsText}
+  className="text-white flex items-center space-x-2 hover:text-green-200 mt-4 transition duration-200"
+>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+    <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v11.69l3.22-3.22a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 1 1 1.06-1.06l3.22 3.22V3a.75.75 0 0 1 .75-.75Zm-9 13.5a.75.75 0 0 1 .75.75v2.25a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V16.5a.75.75 0 0 1 1.5 0v2.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V16.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+  </svg>
+  <span>Download Key</span>
+</button>
+
+                 
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
